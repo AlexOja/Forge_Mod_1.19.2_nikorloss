119a4b97e37ccf61fe897b00dc19136741710b29
AlexOja <75041443+AlexOja@users.noreply.github.com>
2024-05-09 21:54:22 +0300

    Update ModBlocks.java

--- src/main/java/net/nikorloss/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/nikorloss/tutorialmod/block/ModBlocks.java
@@ -32,27 +32,6 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Block> CHISELED_DEEPSLATE_WARDEN = registerBlock("chiseled_deepslate_warden",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_PILLAR = registerBlock("deepslate_pillar",
-            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_PILLAR_CAPITAL = registerBlock("deepslate_pillar_capital",
-            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_SQUARE_TILES = registerBlock("deepslate_square_tiles",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_SQUARE_TILES_STAIRS = registerBlock("deepslate_square_tiles_stairs",
-            () -> new StairBlock(ModBlocks.DEEPSLATE_SQUARE_TILES.get().defaultBlockState(), BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_SQUARE_TILES_SLAB = registerBlock("deepslate_square_tiles_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_DIAGONAL_BRICKS = registerBlock("deepslate_diagonal_bricks",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEEPSLATE_PILLAR_WALL = registerBlock("deepslate_pillar_wall",
-            () -> new WallBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops()), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> SCULKED_COBBLED_DEEPSLATE = registerBlock("sculked_cobbled_deepslate",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops().lightLevel((p_152651_) -> { return 10; })), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> SCULKED_DEEPSLATE = registerBlock("sculked_deepslate",
-            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops().lightLevel((p_152651_) -> { return 10; })), CreativeModeTab.TAB_BUILDING_BLOCKS);
-
     public static final RegistryObject<LiquidBlock> SCULK_WATER_BLOCK = BLOCKS.register("sculk_water_block",
             () -> new LiquidBlock(ModFluids.SOURCE_SCULK_WATER, BlockBehaviour.Properties.copy(Blocks.WATER).lightLevel((p_152651_) -> { return 12; })));
 
